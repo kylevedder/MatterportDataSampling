ccebacb657d9e141ddf6fab392cd0c2ef897cd7e
Kyle Vedder <kyle.c.vedder@gmail.com>
2021-08-12 23:39:36 -0400

    Cleaned up main file, moving most transform to the save_data.py file

--- main.py
+++ main.py
@@ -16,58 +16,10 @@ import open3d as o3d
 from pyntcloud import PyntCloud
 from scipy.spatial.transform import Rotation
 
-from save_kitti import SaveKitti
+from save_data import SaveData
 
 from pathlib import Path
 import argparse
-import pickle
-
-
-def save_rgb(obs, filename):
-    plt.imshow(obs)
-    plt.savefig(filename + ".png")
-    plt.clf()
-
-
-def gen_3d_corners(center, half_extents):
-    cx, cy, cz = center
-    ex, ey, ez = half_extents
-    corners = []
-    for sx in [-1, 1]:
-        for sy in [-1, 1]:
-            for sz in [-1, 1]:
-                corners.append(np.array([cx + ex * sx, cy + ey * sy, cz + ez * sz, 1]))
-    return np.array(corners).T
-
-
-def corners_to_pixel_pos(points, W, H):
-    scale_arr = np.array([[-W/2, 0],[0, H/2]])
-    translate_arr = np.array([[W/2, H/2]]*8).T
-    return (scale_arr @ points + translate_arr)
-
-
-def pixel_pos_to_bbox(points):
-    max_x, min_x = np.max(points[0]), np.min(points[0])
-    max_y, min_y = np.max(points[1]), np.min(points[1])
-    # left, top, right, bottom format
-    # From https://towardsdatascience.com/kitti-coordinate-transformations-125094cd42fb
-    return np.array([min_x, min_y, max_x, max_y])
-
-
-def object_to_image_bbox(obj, rgb_obs, sensor_state, sensor_hfov_deg):
-    W, H, _ = rgb_obs.shape
-    bb = obj.obb
-    _, T_camera_world = gen_cam_frame_transform_matrices(sensor_state)
-    T_image_camera, _ = gen_cam_projection_matrices(sensor_hfov_deg)
-
-    c = gen_3d_corners(bb.center, bb.half_extents)
-    # Convert corners from world frame to camera frame to image space
-    c = (T_image_camera @ T_camera_world @ c)
-    # Normalize by depth in X, Y, Depth frame to get coordinate on image
-    c = c[:2] / c[2]
-    c = corners_to_pixel_pos(c, W, H)
-    c = pixel_pos_to_bbox(c)
-    return c
 
 
 def gen_second_transformation(num_dim):
@@ -76,7 +28,8 @@ def gen_second_transformation(num_dim):
     T_second_camera = np.eye(num_dim)
     T_second_camera[[0,1,2]] = T_second_camera[[2,0,1]]
     T_second_camera[0] *= -1
-    return T_second_camera
+    T_camera_second = np.linalg.inv(T_second_camera)
+    return T_second_camera, T_camera_second
         
 
 def gen_cam_projection_matrices(sensor_hfov_deg):
@@ -103,30 +56,7 @@ def gen_cam_frame_transform_matrices(sensor_state):
     return T_world_camera, T_camera_world
 
 
-def transform_second_frame(data):
-    return gen_second_transformation(data.shape[0]) @ data
-
-
-def transform_camera_frame(data, sensor_state):
-    x, y, z = data
-    _, T_camera_world = gen_cam_frame_transform_matrices(sensor_state)
-    arr = T_camera_world @ np.array([[x, y, z, 1]]).T
-    return arr.T[0,:3]
-
-
-def make_bboxes(objs, rgb_obs, sensor_hfov_deg, sensor_state):
-    names = []
-    bboxes = []
-    corners_lst = []
-    for obj in objs:
-        names.append(obj.category.name())
-        bboxes.append(obj.obb)
-        corners_lst.append(object_to_image_bbox(obj, rgb_obs, sensor_state, sensor_hfov_deg))
-   
-    return names, bboxes, corners_lst
-
-
-def make_pc(depth_obs, T_camera_world, T_second_camera):
+def make_pc(depth_obs, T_second_camera):
     # From https://aihabitat.org/docs/habitat-api/view-transform-warp.html
     # Assumes width and height are same for simplicity.
     W, H, d = depth_obs.shape
@@ -145,12 +75,7 @@ def make_pc(depth_obs, T_camera_world, T_second_camera):
     xys = xys.reshape(4, -1)  # Flatten to 4 by (W*H) matrix.
     # Project to camera frame 3D XYZ from 2.5D pixel X, Pixel Y, Depth, 
     # then project into SECOND frame.
-    return T_second_camera @ T_camera_world @ xys
-
-def obj_in_height_range(obj, depth_sensor_state):
-    x, y, z = transform_camera_frame(obj.obb.center, depth_sensor_state)
-    return (y >= -1 and y <= 1)
-
+    return T_second_camera @ xys
 
 
 def main(dataset_folder, desired_object):
@@ -162,7 +87,7 @@ def main(dataset_folder, desired_object):
         print("Agent acting inside environment.")
         observations = env.reset()
 
-        save_kitti = SaveKitti(dataset_folder)
+        save_kitti = SaveData(dataset_folder)
         episode_idx = 0
         while episode_idx < 55:
             # observations = env.reset()
@@ -172,7 +97,7 @@ def main(dataset_folder, desired_object):
             depth_hfov = config.SIMULATOR.DEPTH_SENSOR.HFOV
 
             T_image_camera, T_camera_image = gen_cam_projection_matrices(depth_hfov)
-            T_second_camera = gen_second_transformation(4)
+            T_second_camera, T_camera_second = gen_second_transformation(4)
             T_world_camera, T_camera_world = gen_cam_frame_transform_matrices(depth_sensor_state)
 
             # Each id in the semantic image is an instance ID, which means we can lookup 
@@ -184,34 +109,30 @@ def main(dataset_folder, desired_object):
             # https://aihabitat.org/docs/habitat-sim/habitat_sim.geo.OBB.html
             distinct_objects = [instance_id_to_obj[instance_id] for instance_id in set(observations["semantic"].flatten())]
 
-            filtered_objects = [obj for obj in distinct_objects if obj.category.name() == desired_object]
-            num_matching_objects = len(filtered_objects)
-            filtered_objects = [obj for obj in filtered_objects if obj_in_height_range(obj, depth_sensor_state)]
-            print("Num removed objects:", num_matching_objects - len(filtered_objects))
+            filtered_objects = [obj for obj in distinct_objects if obj.category.name() == desired_object]            
             if len(filtered_objects) <= 0:
+                print("No such objects found in scene")
+                continue
+            
+            names = [obj.category.name() for obj in filtered_objects]
+            bboxes = [obj.obb for obj in filtered_objects] 
+            pc = make_pc(observations["depth"], 
+                         T_second_camera)
+            num_saved_boxes = save_kitti.save_instance(episode_idx, 
+                                                       observations["rgb"], 
+                                                       names, 
+                                                       bboxes, 
+                                                       pc, 
+                                                       T_second_camera, 
+                                                       T_camera_second, 
+                                                       T_camera_world, 
+                                                       T_image_camera)
+
+            if num_saved_boxes <= 0:
+                print("No boxes met filter criteria")
                 continue
 
-            print(f"Episode {episode_idx} has {len(filtered_objects)} {desired_object}")
-            
-
-            names, bboxes, image_bboxes = make_bboxes(filtered_objects,
-                                              observations["rgb"],
-                                              depth_hfov,
-                                              depth_sensor_state)
-            pc = make_pc(observations["depth"], 
-                         T_camera_world, 
-                         T_second_camera)
-
-
-            save_kitti.save_instance(episode_idx, 
-                                     observations["rgb"], 
-                                     names, 
-                                     bboxes, 
-                                     image_bboxes, 
-                                     pc, 
-                                     T_second_camera, 
-                                     T_camera_world, 
-                                     T_camera_image)
+            print(f"Episode {episode_idx} has {len(filtered_objects)} of {desired_object}")
             episode_idx += 1
         
         print("Done")
---
+++ save_data.py
@@ -0,0 +1,151 @@
+import numpy as np
+from scipy.spatial.transform import Rotation
+import open3d as o3d
+import pandas as pd
+from pyntcloud import PyntCloud
+from pathlib import Path
+import joblib
+import os
+import matplotlib.pyplot as plt
+
+class SaveData:
+  def __init__(self, root_path):
+    self.root_path = root_path
+
+  def _points_to_box(self, points, idx, num):
+    assert points.shape == (8, 3)
+    height = points[:, 2].max() - points[:, 2].min()
+
+    def extract_four_corners(points):
+      duped_points = list(points[:, :2])
+      safe_idx = 0
+      while safe_idx < len(duped_points):
+        safe_val = duped_points[safe_idx]
+        for query_idx in range(safe_idx + 1, len(duped_points)):
+          query_val = duped_points[query_idx]
+          if np.linalg.norm(safe_val - query_val, 1) < 0.01:
+            # Duplicate detected
+            duped_points.pop(query_idx)
+            break
+        safe_idx += 1
+
+      assert len(duped_points) == 4
+      return np.array(duped_points)
+    
+    center3d = np.mean(points, axis=0)
+    corners = extract_four_corners(points)
+    center = np.mean(corners, axis=0)
+    closest_x_point_idx = np.argmin(corners[:, 0])
+    closest_x_point = corners[closest_x_point_idx]
+
+    mask = np.ones(4, bool)
+    mask[closest_x_point_idx] = False
+    other_corners = corners[mask]
+    corner_distances = np.linalg.norm(other_corners - np.array([closest_x_point] * 3), 2, axis=1)
+    short_side_corner = other_corners[np.argmin(corner_distances)]
+
+    short_side_len = np.linalg.norm(closest_x_point - short_side_corner, 2)
+    short_side_center = (short_side_corner - closest_x_point) / 2 + closest_x_point
+    long_side_len = np.linalg.norm(center - short_side_center, 2) * 2
+
+    delta_x, delta_y = (center - short_side_center)
+    yaw = np.rad2deg(np.arctan2(delta_y, delta_x))
+
+    return center3d, long_side_len, short_side_len, height, yaw
+
+
+  def _is_valid_box(self, center):
+    if center[2] < -1 or center[2] > 1:
+      print("outside Z", center[2])
+      return False
+    if np.linalg.norm(center[:2], 2) > 8:
+      print("outside max range", np.linalg.norm(center[:2], 2))
+      return False
+    return True
+
+  
+  def _second_corners_to_image_bb(self, second_corners, rgb_img, T_image_second):
+    W, H, _ = rgb_img.shape
+    assert second_corners.shape == (8, 3)
+    second_corners = np.concatenate([second_corners, np.ones((8, 1))], axis=1)
+    c = T_image_second @ second_corners.T
+
+    def corners_to_pixel_pos(points, W, H):
+      scale_arr = np.array([[-W/2, 0],[0, H/2]])
+      translate_arr = np.array([[W/2, H/2]]*8).T
+      return (scale_arr @ points + translate_arr)
+
+    def pixel_pos_to_bbox(points):
+        max_x, min_x = np.max(points[0]), np.min(points[0])
+        max_y, min_y = np.max(points[1]), np.min(points[1])
+        # left, top, right, bottom format
+        return np.array([min_x, min_y, max_x, max_y])
+
+    c = c[:2] / c[2]
+    c = corners_to_pixel_pos(c, W, H)
+    c = pixel_pos_to_bbox(c)
+    return c
+
+    
+
+  def _save_boxes(self, names, bboxes, rgb_img, T_second_world, T_image_second, idx):
+    mesh = o3d.geometry.TriangleMesh()
+    num_valid_boxes = 0
+    boxes = []
+    for name, bb in zip(names, bboxes):
+      # Bounding boxes are not stored in l,w,h format; they are stored in decending order
+      # side lengths.
+      # Use open3d to transform corners into 3D bounding box, then extract BEV-based info
+      # for the bounding box
+      box = o3d.geometry.TriangleMesh.create_box(*(bb.half_extents * 2))
+      box.rotate(Rotation.from_quat(list(bb.rotation)).as_matrix(), box.get_center())
+      box.translate(tuple(bb.center), relative=False)
+      box.transform(T_second_world)
+      center, l, w, h, yaw = self._points_to_box(np.asarray(box.vertices), idx, num_valid_boxes)
+      if not self._is_valid_box(center):
+        continue
+      box = o3d.geometry.TriangleMesh.create_box(l, w, h)
+      box.rotate(Rotation.from_euler('z', yaw, degrees=True).as_matrix(), box.get_center())
+      box.translate(tuple(center), relative=False)
+      img_bb = self._second_corners_to_image_bb(np.asarray(box.vertices), rgb_img, T_image_second)
+      mesh += box
+      boxes.append(np.array([name, *img_bb, *center, l, w, h, yaw]))
+    
+    boxes = np.array(boxes)
+    if boxes.shape[0] > 0:
+      p = Path(self.root_path + "/boxes/")
+      p.mkdir(parents=True, exist_ok=True)
+      with (p / f"{idx:06d}.txt").open('wb') as f:
+        joblib.dump(boxes, f)
+      o3d.io.write_triangle_mesh(str(p / f"{idx:06d}.ply"), mesh)
+    return boxes
+        
+
+  def _save_pc(self, pc, idx):
+    kMaxHeightCutoff = 2
+    p = Path(self.root_path + "/pc/")
+    p.mkdir(parents=True, exist_ok=True)
+    pc = pc.T[pc[2] < kMaxHeightCutoff].T
+    PyntCloud(pd.DataFrame(data=pc[:3].T,
+        columns=["x", "y", "z"]))\
+        .to_file(str(p / f"{idx:06d}.ply"))
+    # Save in KITTI PC binary format of X, Y, Z, Intensity, with Intensity always 1.
+    pc[3] = 1
+    with (p / f"{idx:06d}.bin").open("wb") as bin_f:
+      joblib.dump(pc.ravel().astype(np.float32), bin_f)
+
+
+  def save_instance(self, idx, rgb_img, names, bboxes, pc, T_second_camera, T_camera_second, T_camera_world, T_image_camera):
+    assert T_image_camera.shape == (4, 4)
+    assert T_camera_world.shape == (4, 4)
+    boxes = self._save_boxes(names, 
+                             bboxes, 
+                             rgb_img, 
+                             T_second_camera @ T_camera_world, 
+                             T_image_camera @ T_camera_second, 
+                             idx)
+    if boxes.shape[0] > 0:
+      self._save_pc(pc, idx)
+    return boxes.shape[0]
+
+
